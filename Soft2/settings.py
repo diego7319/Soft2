f86dd2a21cfb59fb8060dc7fd83cccc1b50dc654
diego7319 <diego7319@gmail.com>
2018-01-14 02:08:42 -0500

    Front end log in

--- Soft2/settings.py
+++ Soft2/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Soft2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['users/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+#STATIC_URL = '/static/'
+
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
