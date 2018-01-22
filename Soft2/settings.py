b36369537ad99d517628c4e7856c8af3cf1bc03b
diego7319 <diego7319@gmail.com>
2018-01-21 21:18:45 -0500

    static fix

--- Soft2/settings.py
+++ Soft2/settings.py
@@ -120,9 +120,15 @@ USE_TZ = True
 
 #STATIC_URL = '/static/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 LOGIN_REDIRECT_URL = 'home'
