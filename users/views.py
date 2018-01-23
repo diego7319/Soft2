36bbdee21d50949d0350115f5891fa72cc5e283f
diego7319 <diego7319@gmail.com>
2018-01-23 15:55:23 -0500

    'index ->signup sign in'

--- users/views.py
+++ users/views.py
@@ -9,15 +9,34 @@ from users.models import Usuario
 
 
 def index(request):
-    formaLogin = Login(request.POST or None)
-    formaSignup = Signup(request.POST or None)
-    context = {
+
+    if request.method == 'POST':
+
+        if request.POST.get('submit') == 'login':
+            if formaLogin.is_valid():
+                datos = formaLogin.cleaned_data
+                rusername=datos.get('user')
+                rpassword= datos.get('password')
+                user = authenticate(username=rusername, password=rpassword)
+                login(request, user)
+                return redirect('home')
+
+        elif request.POST.get('submit') == 'signup':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                login(request, user)
+                return redirect('home')
+    else:
+        formaLogin = Login(request.POST or None)
+        formaSignup = Signup(request.POST or None)
+        context = {
         'formaL': formaLogin,
         'formaS': formaSignup
-    }
-    if request.method == "POST":
-        pass
-    else:
+        }
         return render(request, 'index.html', context)
 
 
