447113d71f1876f26e5c52ebfd9e6c25dd4e9aa2
W3230 <W3230@lab.aca>
2018-01-23 08:29:08 -0500

    'login'

--- users/urls.py
+++ users/urls.py
@@ -10,7 +10,7 @@ from users.views import index,login,signup
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('login/', login , name='login'),
+    #path('login/', login , name='login'),
     #path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     #path('signup/', core_views.signup, name='signup'),
     path('index/', index, name='index')
