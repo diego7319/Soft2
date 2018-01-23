bcc6f8e8992c620563d560c9f359cd50696acd4d
W3230 <W3230@lab.aca>
2018-01-23 08:35:36 -0500

    'viewsindex'

--- Soft2/urls.py
+++ Soft2/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
+#from django.contrib.auth import views as auth_views
 
 
 
