d07ff09b2f591ec8d98b30fd11152fa9f22684e3
diego7319 <diego7319@gmail.com>
2018-01-23 23:36:36 -0500

    borrar custom model

--- users/models.py
+++ users/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
-)
-
-class Usuario(models.Model):
-    user = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-    def __str__(self):
-        return self.user
