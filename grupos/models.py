fed91ca922532ae3871206b3ed9fadd6b741dafe
diego7319 <diego7319@gmail.com>
2018-01-27 23:06:18 -0500

    modelos grupo

--- grupos/models.py
+++ grupos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class grupo(models.Model):
+    nombre = models.CharField(max_length=30,unique=True)
+    owner=models.CharField(max_length=30)
+    def __str__(self):
+        return "%s %s" % (self.nombre)
+
+class Invitacion(models.Model):
+    invitado = models.CharField(max_length=10)
+    owner = models.CharField(max_length=300)
+    grupo = models.ManyToManyField(grupo)
+    def __str__(self):
+        return self.headline
--- grupos/views.py
+++ grupos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User #, Group
+from grupos.models import grupo,Invitacion
 # Create your views here.
+
+def agregargrupo(request):
+
+
+def listausuarios(grupo):
+
+    return HttpResponse()
+
+def invitaciones():
