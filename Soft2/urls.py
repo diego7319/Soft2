f2911d27676edd6c3dd1444314e25827953b00f2
diego7319 <diego7319@gmail.com>
2018-02-04 17:09:32 -0500

    trivia , url agregado, web base

--- Soft2/urls.py
+++ Soft2/urls.py
@@ -25,6 +25,7 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-    path('', include('grupos.urls'))
+    path('', include('grupos.urls'),
+    path('', include('trivia.urls'))
 
 ]
