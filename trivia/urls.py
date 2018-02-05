745176466144aa76834bfa905a5ee9f42ca44b8f
diego <diego7319@gmail.com>
2018-02-04 19:01:21 -0500

    fix urls trivia

--- trivia/urls.py
+++ trivia/urls.py
@@ -2,6 +2,6 @@ from trivia.views import trivia, score
 from django.urls import path
 
 urlpatterns = [
-    path('trivia', trivia, name='trivia'),
+    path('trivia/', trivia, name='trivia'),
     path('score/', score, name='score')
 ]
