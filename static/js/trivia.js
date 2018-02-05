a5d8c5ed51976541bc64f618b6e8f290849aa970
Reptarking <jdsaquico@hotmail.com>
2018-02-04 23:33:59 -0500

    guardar db score funciona

--- static/js/trivia.js
+++ static/js/trivia.js
@@ -1,3 +1,35 @@
+/**
+ * setup JQuery's AJAX methods to setup CSRF token in the request before sending it off.
+ * http://stackoverflow.com/questions/5100539/django-csrf-check-failing-with-an-ajax-post-request
+ */
+
+function getCookie(name)
+{
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+$.ajaxSetup({
+     beforeSend: function(xhr, settings) {
+         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+             // Only send the token to relative URLs i.e. locally.
+             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+         }
+     }
+});
+
 function obtenerpregunta(){
 
     $.get("../mostrarpregunta/",
@@ -10,7 +42,7 @@ function obtenerpregunta(){
         document.getElementById("d1").innerHTML=data.d1
         document.getElementById("d2").innerHTML=data.d2
         document.getElementById("d3").innerHTML=data.d3
-    });
+    });document.getElementById("rpta").innerHTML=''
 };
 
 
@@ -26,9 +58,11 @@ parametros={'pregunta':pregunta,'respuesta':respuesta}
               type: "POST",
               url: '../score/',
               data: parametros,
+
               success: function(data)
               {
-                'haer algo data'
+
+            document.getElementById("rpta").innerHTML=data.resultado
               }
           });
-       });
+       };
--- trivia/views.py
+++ trivia/views.py
@@ -1,43 +1,11 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.http import JsonResponse
-from trivia.models import PreguntaTrivia
+from trivia.models import PreguntaTrivia,scoretrivia
 from random import randint,shuffle
 
 # Create your views here.
-"""
-def trivia(request):
-    context = {
-        'listapreguntas': preguntas
-    }
-    return render(request, 'main.html')#, context)
 
-def score(request):
-    acum = 0
-    for x in preguntas():
-        if x['opciones'][x['respuesta']] == request.POST.get(x['id'],''):
-            acum += 100
-    return render(request, 'score.html', {'score':acum})
-
-
-def preguntas():
-    preguntas = [
-        {
-            'id':'Pregunta1',
-            'descripcion':'Primer mundial',
-            'opciones':['0','1','2','3','4'],
-            'respuesta':1
-        },
-        {
-            'id':'Pregunta2',
-            'descripcion':'Segundo mundial',
-            'opciones':['5','6','7','8'],
-            'respuesta':0
-        }
-    ]
-    return preguntas
-
-"""
 def templated(request):
     if not request.user.is_authenticated:
         return redirect('index')
@@ -63,7 +31,19 @@ def mostrarpregunta(request):
 
 #guarda la respuesta en la base de datos
 def score(request):
-    respuesta=request.POST.get('pregunta')
-    pregunta=request.POST.get('respuesta')
-    print (respuesta)
-    pass
+    print(request.user.username)
+    respuesta=request.POST.get('respuesta')
+    pregunta=request.POST.get('pregunta')
+    objpregunta=PreguntaTrivia.objects.get(descPregunta=pregunta)
+    print ('PREGUNTA DE DB  '+ objpregunta.descPregunta)
+    rpuntaje='0'
+    jsonrespuesta={'resultado':'Respuesta incorrecta'}
+    if objpregunta.respuesta==respuesta:
+        print('CORRECTA')
+        rpuntaje='1'
+        jsonrespuesta['resultado']='Respuesta correcta'
+    nuevoscore=scoretrivia(idPregunta=objpregunta.idPregunta,
+    user=request.user.username,puntaje=rpuntaje);
+    nuevoscore.save()
+
+    return JsonResponse(jsonrespuesta)
