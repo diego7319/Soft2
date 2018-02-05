bab17b6674c87081fdabaf68f35abaf76141d727
Antony395 <20131796@aloe.ulima.edu.pe>
2018-02-04 21:46:36 -0500

    mostrar pregunta funciona, falta css

--- trivia/urls.py
+++ trivia/urls.py
@@ -2,8 +2,7 @@ from trivia.views import trivia, score,mostrarpregunta,templated
 from django.urls import path
 
 urlpatterns = [
-    path('trivia/', trivia, name='trivia'),
+    path('trivia/', templated, name='templated'),
     path('score/', score, name='score'),
-    path('mostrarpregunta/',mostrarpregunta,name='mostrarpregunta'),
-    path('templated/', templated, name='templated'),
+    path('mostrarpregunta/',mostrarpregunta,name='mostrarpregunta')
 ]
