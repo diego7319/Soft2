2250201b2ae05b4161ee8f4d3c8b6a38905c1f9f
Antony395 <20131796@aloe.ulima.edu.pe>
2018-02-04 21:39:44 -0500

    js , holihtml fix

--- static/js/trivia.js
+++ static/js/trivia.js
@@ -3,10 +3,12 @@ function obtenerpregunta(){
     $.get("../mostrarpregunta/",
 
     function(data){
-        var parsed = $.parseJSON(data);
-        console.log(parsed)
-        document.getElementById("pregunta").innerHTML = 'a';
+        console.log()
 
-
+        document.getElementById("pregunta").innerHTML = data.pregunta;
+   document.getElementById("d0").innerHTML=data.d0
+        document.getElementById("d1").innerHTML=data.d1
+        document.getElementById("d2").innerHTML=data.d2
+        document.getElementById("d3").innerHTML=data.d3
     });
 };
