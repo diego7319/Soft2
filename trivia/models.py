84dee7855e6cb13bb35dd179025eb5672d7550e9
Antony395 <20131796@aloe.ulima.edu.pe>
2018-02-04 19:36:26 -0500

    modelo score user grupo

--- trivia/models.py
+++ trivia/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from grupos import models
+
 #Preguntas
 class PreguntaTrivia(models.Model):
     idPregunta = models.AutoField(primary_key=True)
@@ -9,3 +11,8 @@ class PreguntaTrivia(models.Model):
     incorrecta3=models.CharField(max_length=150)
     def __str__(self):
         return "%s %s" % (self.descPregunta)
+
+class scoretrivia(models.Model):
+    grupo=models.CharField(max_length=30)
+    idPregunta = models.CharField(max_length=1000)
+    user=models.CharField(max_length=20)
--- trivia/views.py
+++ trivia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from grupos import models
 # Create your views here.
 def trivia(request):
     context = {
