634d07c8b7ef3f456e00a792af2e130692b2ed11
Reptarking <jdsaquico@hotmail.com>
2018-02-04 22:26:35 -0500

    descomentar modelscoretrivia

--- trivia/models.py
+++ trivia/models.py
@@ -17,12 +17,10 @@ class PreguntaTrivia(models.Model):
     incorrecta3=models.CharField(max_length=150)
     def __str__(self):
         return "%s %s" % (self.descPregunta)
-'''
+
 class scoretrivia(models.Model):
     idPregunta = models.AutoField(primary_key=True)
     grupo=models.CharField(max_length=30)
     idPregunta = models.CharField(max_length=1000)
     user=models.CharField(max_length=20)
     puntaje=models.CharField(max_length=2)
-    #1 respuestacorrecta, 0 incorrecta
-'''
