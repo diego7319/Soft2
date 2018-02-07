1fdb5f269d5330b389d5806ca80863ef75ae4f16
Reptarking <jdsaquico@hotmail.com>
2018-02-07 16:13:05 -0500

    borrado boton siguiente

--- users/models.py
+++ users/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+class Cuenta(models.Model):
+    usuario = models.CharField(max_length=30,unique=True)
+    dinerocuenta =models.CharField(max_length=10)
+    def __str__(self):
+        return "%s %s" (self.usuario,self.dinerocuenta)
