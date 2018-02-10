7002545fabe389ceba78fc8d1e946adfa353d85e
Reptarking <diego7319@gmail.com>
2018-02-09 22:34:58 -0500

    agregado tabla cuenta de usuario a admin

--- users/models.py
+++ users/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Cuenta(models.Model):
     usuario = models.CharField(max_length=30,unique=True)
-    dinerocuenta =models.CharField(max_length=10)
+    dinerocuenta =models.FloatField(default=0)
     def __str__(self):
         return "%s %s" (self.usuario,self.dinerocuenta)
