322590bf25051b4fe1b21de8614af98da5b79031
Reptarking <diego7319@gmail.com>
2018-02-09 23:24:11 -0500

    fix

--- users/views.py
+++ users/views.py
@@ -93,7 +93,8 @@ def pRegistro(request):
             try:
                 user = User.objects.create_user(username=raw_username,password=raw_password)
                 user.save();
-                cuenta=usuariocuenta.objects,create_user(username=raw_username,dinerocuenta=10)
+                cuenta=usuariocuenta.objects.create_user(username=raw_username,dinerocuenta=10)
+                cuenta.save()
                 context['estadoregistro']='Registrado correctamente'
                 return render(request,'index.html',context)
             except:
