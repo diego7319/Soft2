7477cddccdd7cea72ca3be5b2e8a1147671684c7
diego7319 <diego7319@gmail.com>
2018-02-18 17:06:36 -0500

    modelo partidos

--- equipoideal/models.py
+++ equipoideal/models.py
@@ -4,6 +4,13 @@ from django.db import models
 
 class JugadorPais(models.Model):
     nombre = models.CharField(max_length=70)
-    pais=models.CharField(max_length=70)
+    pais = models.CharField(max_length=70)
     def __str__(self):
         return "%s %s" % (self.nombre,self.pais)
+
+class Partido(models.Model):
+    equipo1 = models.CharField(max_length=30)
+    equipo2 = models.CharField(max_length=30)
+    fecha = models.DateField()
+    def __str__(self):
+        return "%s %s %s" % (self.equpo1,self.equipo2,self.fecha)
