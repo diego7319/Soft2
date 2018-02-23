1e4709b1392ff6df4dc8d5669142292701035ff3
diego7319 <diego7319@gmail.com>
2018-02-23 18:44:48 -0500

    fix

--- equipoideal/views.py
+++ equipoideal/views.py
@@ -99,8 +99,8 @@ def obtenerSalasEI(request):
             jsonrespuesta[str(cont)]={'sala':i.split('-')[0],'grupo':i.split('-')[1],'estado':estadopago(i.split('-')[0],i.split('-')[1],ruser)}
             cont+=1
         except :
-            PagoSalaEI(nombreJuego=i.split('-')[0],grupo=i.split('-')[1],user=ruser,estadopago='deuda')
-            PagoSalaEI.save()
+            fixinv=PagoSalaEI(nombreJuego=i.split('-')[0],grupo=i.split('-')[1],user=ruser,estadopago='deuda')
+            fixinv.save()
             jsonrespuesta[str(cont)]={'sala':i.split('-')[0],'grupo':i.split('-')[1],'estado':estadopago(i.split('-')[0],i.split('-')[1],ruser)}
             cont+=1
     return JsonResponse(jsonrespuesta)
